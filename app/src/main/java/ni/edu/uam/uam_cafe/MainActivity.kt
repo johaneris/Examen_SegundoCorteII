e274f5fae3afcc07dc4493e8e41dd3d86d5773e5
SteMt323 <stevenmt@uamv.edu.ni>
2026-05-20 20:29:38 -0600

    Push: enlazando al archivo principal

--- app/src/main/java/ni/edu/uam/uam_cafe/MainActivity.kt
+++ app/src/main/java/ni/edu/uam/uam_cafe/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import ni.edu.uam.uam_cafe.navigation.AppNavigation
 import ni.edu.uam.uam_cafe.ui.theme.Uam_CafeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Uam_CafeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Uam_CafeTheme {
-        Greeting("Android")
     }
 }
